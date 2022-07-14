e4002c3cd9a5c6932cc490e3d62c1f54f8b5dec5
FraserNyale <nyalefraser@gmail.com>
2022-07-14 00:47:29 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,30 @@
 #include "main.h"
-/**
-  * _strncat- a function to cat 2 strings by n
-  * @dest: we append to
-  * @src: we appends from
-  * @n: we append by
-  *
-  * Return: char *
-  */
-char *_strncat(char *dest, char *src, int n)
-{
-	char *begin = dest;
 
-	while (*dest)
-		dest++;
+/**
+ * _strncat- a function cat 2 strings by n
+ * @dest: we append to
+ * @src: we appends from
+ * @n: we append by
+ *
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	char *begin = dest
+		
+		while (*dest)
+			dest++;
 	for (; n > 0; n--)
 	{
 		if (*src)
 		{
 			*dest = *src;
 			dest++;
-			src++;
+			scr++;
 		}
 		else
 			break;
 	}
-return (begin);
+	return (begin);
 }
+
