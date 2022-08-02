571aaa743a73f81b550b992d79d5f634f1969133
FraserNyale <106987986+FraserNyale@users.noreply.github.com>
2022-08-02 19:46:41 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
@@ -9,4 +6,3 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
->>>>>>> 57ec61a5a966c7739616e8a7d22618acb4c0d77d
